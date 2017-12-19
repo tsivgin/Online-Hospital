682885073582e544f0b7fc471d65085b7c7f7e62
ByMurat8 <33281771+ByMurat8@users.noreply.github.com>
2017-12-19 15:09:14 +0300

    kod

--- Online-Hospital/Online-Hospital/login.aspx.cs
+++ Online-Hospital/Online-Hospital/login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace Online_Hospital
 {
@@ -12,6 +15,62 @@ namespace Online_Hospital
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            object user = Session["tc_no"];
+            if (user != null)
+            {
+
+
+                //Response.Redirect("about.aspx");
+                pnlgiris.Visible = false;
+                pnluser.Visible = true;
+                lblusername.Text = user.ToString();
+            }
+            else
+            {
+                pnlgiris.Visible = true;
+                pnluser.Visible = false;
+            }
         }
+
+        protected void loginbtn_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection.ClearAllPools();
+            SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+            cnn.Open();
+            string sorgu = "Select * From person where Tc_no=@tc_no AND password=@password";
+
+            SqlCommand cmd = new SqlCommand(sorgu, cnn);
+            cmd.Parameters.AddWithValue("@tc_no", TC.Text);
+            cmd.Parameters.AddWithValue("@password", Password.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+
+                Session.Timeout = 300;
+                Session.Add("tc_no", dr["Tc_no"].ToString());
+
+                lblusername.Text = dr["role"].ToString().Trim();
+                if (lblusername.Text == "admin")
+                {
+                    Response.Redirect("Admin/default.aspx");
+                }
+                Response.Redirect("Patient/default.aspx");
+
+            }
+            cnn.Close();
+
+
+
+        }
+
+        protected void btnlogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect(Request.RawUrl);
+        }
+
+
     }
 }
