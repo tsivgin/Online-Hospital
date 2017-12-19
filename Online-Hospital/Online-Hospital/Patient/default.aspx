5d3dbea0e5374890dca6bb6147938e2d5d15f8c9
tsivgin <33281775+tsivgin@users.noreply.github.com>
2017-12-19 14:33:09 +0200

    edit

---
+++ Online-Hospital/Online-Hospital/Patient/default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Patient/patient.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Online_Hospital.Patient._default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Online-Hospital/Online-Hospital/Patient/patient.Master.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Online_Hospital.Patient
+{
+    public partial class patient : System.Web.UI.MasterPage
+    {
+        Database db = new Database();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                object tc_no = Session["tc_no"];
+                if (tc_no != null)
+                    PopulateData();
+                else
+                    Response.Redirect("../login.aspx");
+            }
+        }
+
+        protected void PopulateData()
+        {
+            DataRow dr;
+            String tc_no = Session["tc_no"].ToString();
+            SqlCommand cmd;
+            String query = "Select * from person where Tc_no = '" + tc_no + "'";
+            cmd = db.MySqlCommand(query);
+            dr = db.SelectData(cmd);
+            ltrl_Username.Text = dr["Name"].ToString();
+            ltrl_Username.Visible = true;
+
+
+
+
+
+        }
+
+        protected void LogOut(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("../login.aspx");
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
